b8a0eba5e01f3abe640fcc7fce55c0e36e373edc
Carolina Samaca <carolinasamacagonzalez@gmail.com>
2021-08-20 18:57:28 -0500

    actualizacion responsive

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const navToggle = document.querySelector(".nav-toggle");
 const navMenu = document.querySelector(".nav-menu");
 
 navToggle.addEventListener("click", () => {
-    navMenu.classList.toggle("nav-menu_visible");
+    navMenu.classList.toggle(
+        "nav-menu_visible"
+    ); /* cuando se le hace click en el menu lo cierra o lo abre */
 
     if (navMenu.classList.contains("nav-menu_visible")) {
         navToggle.setAttribute("aria-label", "Cerrar menú");
@@ -10,3 +12,9 @@ navToggle.addEventListener("click", () => {
         navToggle.setAttribute("aria-label", "Abrir menú");
     }
 });
+/* toggle si esta la clase la elimina y si no esta la añade cuando se realize el evento click a toggles (icono hamgurguesa) le va añadir o quitar la clase nav menu_viible a nav menu */
+/* cuando nav class active esta se muestra el menu */
+/* contains para valores Boolean */
+/* es los condicionales si nav menu tiene la clase nav-menu_visible el arial label dira cerrar en caso contrario le dira abrir
+ */
+/*  lo que hace setatribute es cambiar la constante luego recibe dos valores el primero es el atributo que quiero cambiar y el segundo el valor de ese elemento */
