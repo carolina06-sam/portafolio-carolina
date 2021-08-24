6842930b12f5d192203f23bf859f9bbab903d11f
Carolina Samaca <carolinasamacagonzalez@gmail.com>
2021-08-23 21:00:12 -0500

    cambios en modo responsive

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const navToggle = document.querySelector(".nav-toggle");
 const navMenu = document.querySelector(".nav-menu");
+const navInicio = document.querySelectorAll(".nav-menu-item");
 
 navToggle.addEventListener("click", () => {
-    navMenu.classList.toggle(
-        "nav-menu_visible"
-    ); /* cuando se le hace click en el menu lo cierra o lo abre */
+    navMenu.classList.toggle("nav-menu_visible");
+    /* cuando se le hace click en el menu lo cierra o lo abre */
 
     if (navMenu.classList.contains("nav-menu_visible")) {
         navToggle.setAttribute("aria-label", "Cerrar menú");
@@ -12,6 +12,12 @@ navToggle.addEventListener("click", () => {
         navToggle.setAttribute("aria-label", "Abrir menú");
     }
 });
+
+navMenu.addEventListener("click", () => {
+    navMenu.classList.toggle("nav-menu_visible");
+    /* cuando se le hace click en el menu lo cierra o lo abre */
+});
+
 /* toggle si esta la clase la elimina y si no esta la añade cuando se realize el evento click a toggles (icono hamgurguesa) le va añadir o quitar la clase nav menu_viible a nav menu */
 /* cuando nav class active esta se muestra el menu */
 /* contains para valores Boolean */
